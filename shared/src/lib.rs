618aab7dbb79bceaf8192117d5da5772baf7e43f
pedrobartolini <pedrobartolini@outlook.com>
2025-11-03 23:22:50 -0300

    subtle change in mtu

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -8,7 +8,7 @@ pub use ipv4_pool::*;
 pub use ipv6_pool::*;
 pub use udp_id::UdpId;
 
-pub const MTU: u16 = 1600;
+pub const MTU: u16 = 1400;
 
 pub const PACKET_TYPE_FORWARD: u8 = 1;
 pub const PACKET_TYPE_VIRTUAL_ADDRESSES: u8 = 2;
